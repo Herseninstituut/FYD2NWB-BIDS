6437bad119e8427f201ead36479215292541ecfe
chrisvdt <c.vandertogt@nin.knaw.nl>
2024-04-18 14:15:33 +0200

    Create server.m

---
+++ server.m
@@ -0,0 +1,31 @@
+
+  %% SERVER
+
+
+    s = tcpserver("localhost", 8080, "ConnectionChangedFcn", @connectionFcn);   
+    configureCallback(s,"terminator", @readFcn);
+
+    function connectionFcn (src,~)
+        if src.Connected
+           disp("client connection request accepted.")
+        else
+           disp("Client has disconnected.")
+        end
+    end
+    
+    function readFcn(src, ~)
+        message = readline(src);
+        disp(message)
+   end
+
+
+
+%% CLIENT
+
+% client = tcpclient("localhost",8080)
+% 
+% writeline(client, "Hello  world")
+% 
+% writeline(client, '{ "hello": "world" )' )
+% 
+% client = []
