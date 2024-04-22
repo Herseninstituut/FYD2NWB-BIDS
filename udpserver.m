048bd4c8bcd94e12e55ca6940bcb2f275b6a687f
chrisvdt <c.vandertogt@nin.knaw.nl>
2024-04-22 16:25:55 +0200

    Update udpserver.m

--- udpserver.m
+++ udpserver.m
@@ -4,6 +4,22 @@ u = udpport("IPV4", LocalPort=60816);
 configureCallback(u,"terminator", @readudp);
 
 function readudp(src, ~)
-    command = readline(src);
-    disp(command)
+    persistent client port
+    strdata = readline(src);
+    try
+        data = jsondecode(strdata);
+    catch
+        writeline(src, 'Error decoding json input', client, str2double(port))
+        return
+    end
+
+    if isfield(data, 'messg')
+        if matches(data.messg, 'connected')
+            client = data.client;
+            port = data.port;
+            writeline(src, data.messg, client, port)
+        end
+    end
+    disp(data)
+ 
 end
