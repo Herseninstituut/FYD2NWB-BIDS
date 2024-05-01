1e88cf5ee062b55a1034021fe3191b8e816b1960
chrisvdt <c.vandertogt@nin.knaw.nl>
2024-05-01 11:54:07 +0200

    Update getSetup.m

--- DJ/getSetup.m
+++ DJ/getSetup.m
@@ -1,26 +1,26 @@
-function setup_bids = getSetup( setupname )
+function setup_meta = getSetup( setupname )
 
 global dbpar
 
 Database = dbpar.Database;  %= yourlab
 query = eval([Database '.Setups']); % setups table in FYD database for particular lab
 Sel = ['setupid= "' setupname '"'];
-setup_meta = fetch(query & Sel, 'shortdescr', 'longdescr', 'type');
+metadata = fetch(query & Sel, 'shortdescr', 'longdescr', 'type');
 
-if strcmp(setup_meta.type, 'ephys')
+if strcmp(metadata.type, 'ephys')
     query = bids.Ephys;  %ephys table in bids general database
     Sel = ['recording_setup= "' setupname '"'];
-    setup_bids = fetch(query & Sel, '*' );
+    setup_meta = fetch(query & Sel, '*' );
     
-elseif strcmp(setup_meta.type, 'multi_photon')
+elseif strcmp(metadata.type, 'multi_photon')
     query = bids.MultiPhoton;  %ephys table in bids general database
     Sel = ['recording_setup= "' setupname '"'];
-    setup_bids = fetch(query & Sel, '*' );
+    setup_meta = fetch(query & Sel, '*' );
     
 else 
     disp('No type information for this setup! Cannot retrieve bids data.')
 end
 
-setup_bids.shortdescr = setup_meta.shortdescr;
-setup_bids.longdescr = setup_meta.longdescr;
-setup_bids.type = setup_meta.type;
+setup_meta.shortdescr = metadata.shortdescr;
+setup_meta.longdescr = metadata.longdescr;
+setup_meta.type = metadata.type;
