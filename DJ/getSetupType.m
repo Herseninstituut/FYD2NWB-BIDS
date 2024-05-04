f6a19436c7c5230aa816ea97800b458eb426e601
Chris van der Togt <c.vandertogt@nin.knaw.nl>
2024-05-04 08:59:56 +0200

    rearrange files

--- DJ/getSetupType.m
+++
@@ -1,9 +0,0 @@
-function type = getSetupType( setupname )
-
-    global dbpar
-
-    Database = dbpar.Database;  %= yourlab
-    query = eval([Database '.Setups']); % setups table in FYD database for particular lab
-    Sel = ['setupid= "' setupname '"'];
-    setup = fetch(query & Sel, 'type');
-    type = setup.type;
