afd906660508d9963cb493e4b0e60845a35afa66
chrisvdt <c.vandertogt@nin.knaw.nl>
2025-02-07 11:56:03 +0100

    Update set_todo.m

--- NWB/set_todo.m
+++ NWB/set_todo.m
@@ -1,7 +1,6 @@
-%add a session to the todo list
+%% adding a session to the conversion todo list
 
-%adding neccessary folders to your path, 
-
+%Initialize Datajoint for your lab (once per session)
 labs = {'roelfsemalab','leveltlab','heimellab', ...                   
         'lohmannlab','kolelab','willuhnlab', ...
         'socialbrainlab', 'dezeeuwlab', 'vansomerenlab', ...
@@ -15,19 +14,19 @@ initDJ(lab);
 %% Select sessionids based on project, dataset, condition, subject, date, 
 sessions = getSessions(project='', dataset='', subject='');
 
- 
-%% Select a sessionid and check if it contains all neccessary metadata
 % Or simply go to the FYD webapp and select a single sessionid.
 sessionid = 'Amarone_20221013_001';
+
+%% Validate your session metadata
 all_md = getMetadata(sessionid);
 
-%% If so add it to the todo list to automatically be converted to NWB format.
+%% If validated add it to the todo list to automatically be converted to NWB format.
 if ~isempty(all_md)
     query = bids.Nwblist;
     query.insert( struct('sessionid', sessionid, 'lab', lab ) )
 end
+% Check status of conversion at https://nhi-fyd.nin.nl/nwblog.html
 
-%% Check status of conversion at https://nhi-fyd.nin.nl/nwblog.html
-
+%% Manual conversion
 % [conversion_status, nwb, path_nwb] = convert2nwb(all_md);
 % del(query & ['sessionid="' sessionid '"'])
